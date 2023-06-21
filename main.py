ba8e515830b8a8fd41150416d574a4ec246e1230
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2023-06-21 13:37:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import ParseMode, Update
 import logging
-import requests
-import json
-import re
+import instaloader
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     handlers=[logging.StreamHandler()])
 
-TOKEN = "6241362530:AAErqsOBc0BF8H1O7dJyHd_LKaWvFnpHcu8"
+TOKEN = "6241362530:AAHq-3ruJbiYbVRZoT4rAx8hSMGy5tGksq4"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -21,40 +19,17 @@ def download(update: Update, context: CallbackContext):
         pass
 
     if "instagram.com" in instagram_post:
-        shortcode_match = re.search(r"/([A-Za-z0-9_-]{11})/", instagram_post)
-        if shortcode_match:
-            shortcode = shortcode_match.group(1)
-            url = f"https://instagram.com/p/{shortcode}?__a=1"
-            try:
-                visit = requests.get(url).json()
-            except:
-                context.bot.sendMessage(chat_id=update.message.chat_id, text="Invalid Instagram URL")
-                return
-
-            if "graphql" in visit:
-                media_info = visit["graphql"]["shortcode_media"]
-                is_video = media_info.get("is_video", False)
-
-                if is_video:
-                    video_url = media_info.get("video_url")
-                    if video_url:
-                        context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
-                        context.bot.send_video(chat_id=update.message.chat_id, video=video_url)
-                    else:
-                        context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unable to download the video.")
-                else:
-                    photo_url = media_info.get("display_url")
-                    if photo_url:
-                        context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_photo")
-                        context.bot.send_photo(chat_id=update.message.chat_id, photo=photo_url)
-                    else:
-                        context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unable to download the photo.")
-            else:
-                context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unsupported Instagram URL.")
-        else:
+        loader = instaloader.Instaloader()
+        try:
+            shortcode = instaloader.utils.get_shortcode_from_url(instagram_post)
+            loader.download_by_shortcode(shortcode, target="#tmp")
+            file_path = f"#tmp/{shortcode}.mp4"
+            context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
+            context.bot.send_video(chat_id=update.message.chat_id, video=open(file_path, 'rb'))
+        except:
             context.bot.sendMessage(chat_id=update.message.chat_id, text="Invalid Instagram URL")
     else:
-        context.bot.sendMessage(chat_id=update.message.chat_id, text="Kindly Send Me Public Instagram Video/Photo URL")
+        context.bot.sendMessage(chat_id=update.message.chat_id, text="Kindly Send Me Public Instagram Reels URL")
 
 
 def main():
@@ -69,4 +44,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-      
