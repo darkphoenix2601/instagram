271d53b0dcbf2d503da7edb57d1d489039615bf8
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2023-06-21 13:35:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from telegram import ParseMode, Update
 import logging
 import requests
 import json
+import re
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     handlers=[logging.StreamHandler()])
 
-TOKEN = "6241362530:AAHGDYXL3h_utm67WGwcXoMQTiR6IoA-5aU"
+TOKEN = "6241362530:AAErqsOBc0BF8H1O7dJyHd_LKaWvFnpHcu8"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -20,33 +21,38 @@ def download(update: Update, context: CallbackContext):
         pass
 
     if "instagram.com" in instagram_post:
-        url = f"{instagram_post}?__a=1"
-        try:
-            visit = requests.get(url).json()
-        except:
-            context.bot.sendMessage(chat_id=update.message.chat_id, text="Invalid Instagram URL")
-            return
+        shortcode_match = re.search(r"/([A-Za-z0-9_-]{11})/", instagram_post)
+        if shortcode_match:
+            shortcode = shortcode_match.group(1)
+            url = f"https://instagram.com/p/{shortcode}?__a=1"
+            try:
+                visit = requests.get(url).json()
+            except:
+                context.bot.sendMessage(chat_id=update.message.chat_id, text="Invalid Instagram URL")
+                return
 
-        if "graphql" in visit:
-            media_info = visit["graphql"]["shortcode_media"]
-            is_video = media_info.get("is_video", False)
+            if "graphql" in visit:
+                media_info = visit["graphql"]["shortcode_media"]
+                is_video = media_info.get("is_video", False)
 
-            if is_video:
-                video_url = media_info.get("video_url")
-                if video_url:
-                    context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
-                    context.bot.send_video(chat_id=update.message.chat_id, video=video_url)
+                if is_video:
+                    video_url = media_info.get("video_url")
+                    if video_url:
+                        context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_video")
+                        context.bot.send_video(chat_id=update.message.chat_id, video=video_url)
+                    else:
+                        context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unable to download the video.")
                 else:
-                    context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unable to download the video.")
+                    photo_url = media_info.get("display_url")
+                    if photo_url:
+                        context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_photo")
+                        context.bot.send_photo(chat_id=update.message.chat_id, photo=photo_url)
+                    else:
+                        context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unable to download the photo.")
             else:
-                photo_url = media_info.get("display_url")
-                if photo_url:
-                    context.bot.send_chat_action(chat_id=update.message.chat_id, action="upload_photo")
-                    context.bot.send_photo(chat_id=update.message.chat_id, photo=photo_url)
-                else:
-                    context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unable to download the photo.")
+                context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unsupported Instagram URL.")
         else:
-            context.bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, unsupported Instagram URL.")
+            context.bot.sendMessage(chat_id=update.message.chat_id, text="Invalid Instagram URL")
     else:
         context.bot.sendMessage(chat_id=update.message.chat_id, text="Kindly Send Me Public Instagram Video/Photo URL")
 
@@ -63,4 +69,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-          
+      
