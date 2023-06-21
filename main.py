8f71bcffafec5b896754975ff14b040ba5f7c023
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2023-06-21 11:19:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "6241362530:AAGXEHxQ_YNZiZjMHuL5MR1P3W3mXwmvH-4"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
